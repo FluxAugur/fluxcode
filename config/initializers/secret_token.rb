701c52ec4aff52ea7ebb726965c19b8d1e30e1a8
Nathanial L. McConnell <FluxAugur@gmail.com>
2013-10-25 21:33:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fluxcode::Application.config.secret_key_base = 'c2d41645547046192b979582180a93bacbe515afa293f9bd26a6234774da2c026e1a3f5ded8e13c5d71b763c9f50428598bae1f21c961ce2b305c44e13d8df18'
